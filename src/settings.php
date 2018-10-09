e9401669e0480b3d84975f07706f7ecab0139f7f
tiboitel <jules.boitelle@gmail.com>
2018-10-09 20:45:25 +0200

    update

--- src/settings.php
+++ src/settings.php
@@ -1,11 +1,8 @@
 <?php
-
 defined('DS') ?: define('DS', DIRECTORY_SEPARATOR);
 defined('ROOT') ?: define('ROOT', dirname(__DIR__) . DS);
-
 /*if (file_exists(ROOT . '.env')) {
-}*/
-
+ * }*/
 return [
 	'settings' => [
 		'displayErrorDetails'		=> true,
