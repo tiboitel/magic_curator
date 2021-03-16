a06e059fefe2fcdd067b074c2083b6ec880068b6
tboitelle <tboitelle@twipi-group.com>
2021-03-16 12:15:48 +0100

    [FEATURE] Add a script to run easily the data crawler.

---
+++ run-crawler.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+ECHO Start of the application...
+ECHO ---------------------------
+ECHO Lauching crawler...
+cd scripts
+php ./crawler.php
+cd ..
+ECHO 100% done. All data are updated !
+ECHO ----------------------------
